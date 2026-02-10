1b12bd0bffefbe732216e90af45e56538dfeeb3c
DentistGus <cervone.benito@gmail.com>
2026-02-10 15:20:09 +0100

    Rimossi file .vscode e .pio

--- .pio/build/esp32doit-devkit-v1/lib0ec/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib0ec/WebServer/detail/mimetable.cpp.o: \
- C:/Users/cervo/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/cervo/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/cervo/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
